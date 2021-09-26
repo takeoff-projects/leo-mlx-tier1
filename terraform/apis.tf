06725f0d8cf1d058936661049a1e107fdbd8cd54
leo-mlx <leonid.balioshenko@takeoff.com>
2021-09-26 21:57:14 +0300

    terraform debug

--- terraform/apis.tf
+++ terraform/apis.tf
@@ -7,3 +7,8 @@ resource "google_project_service" "api2" {
   service = "datastore.googleapis.com"
   disable_dependent_services = true
 }
+
+resource "google_project_service" "api3" {
+  service = "cloudapis.googleapis.com"
+  disable_dependent_services = true
+}
