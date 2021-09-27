c28a50fdf9ad61591d083e5ca2bc1cd91345a4f0
leo-mlx <leonid.balioshenko@takeoff.com>
2021-09-27 13:32:20 +0300

    Do automation

--- install.sh
+++ install.sh
@@ -1,2 +1,14 @@
-gcloud datastore databases create --region=us-central
-gcloud datastore indexes create index.yaml
+#!/bin/bash
+
+export GOOGLE_CLOUD_PROJECT=roi-takeoff-user7
+cd terrafrom
+#terraform init && terraform apply
+if [ -e main_sa.json ]; then
+	export GOOGLE_APPLICATION_CREDENTIALS=$(pwd)/main_sa.json
+	echo $GOOGLE_APPLICATION_CREDENTIALS
+else
+	echo "Service account not created, we can't continue"
+	exit 1
+fi
+#gcloud datastore databases create --region=us-central
+#gcloud datastore indexes create index.yaml
